2a125e30c1d15fcfdcf059c175d280bfe5bdb2d0
Abirami K <147965516+Abirami-02@users.noreply.github.com>
2026-07-24 21:03:57 +0530

    Add number guessing game functionality
    Implement a number guessing game that provides feedback on user guesses.

--- Number GuessGame.py
+++ Number GuessGame.py
@@ -1,1 +1,30 @@
+print("Guessing Number Game:")
+count=0
+val=0
+guess_num=10
+while val!=guess_num:
+    val=int(input("Enter The Number:"))
+    if val>guess_num:
+        print("Too High")
+        count+=1
+    elif val<guess_num:
+        print("Too Low")
+        count+=1
+    else:
+        print("Yes,Your are Correct")
+        count+=1
+print("User Attempt:",count)
+
+    
+
+
+
+
+        
+ 
+
+
+
+
+      
 
